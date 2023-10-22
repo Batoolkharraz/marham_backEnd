0df6655353fa43efb426df59d44fbbd84a0ee065
Batoolkharraz <99608072+Batoolkharraz@users.noreply.github.com>
2023-10-22 16:32:29 +0300

    Merge branch 'main' of https://github.com/Batoolkharraz/marham_backEnd

--- src/module/Authalaa/signin/signin.js
+++ src/module/Authalaa/signin/signin.js
@@ -1,20 +1,35 @@
 import usermodel from '../DB/Usermodel.js';
 import bcrypt from 'bcrypt';
-import Jwt from 'jsonwebtoken';
-export const signin=async(req,res)=>{
-const{username,password}=req.body;
-const user=await usermodel.findOne({username});
-if(!user)
-{
-    return res.json(false);
-}
-const match=bcrypt.compareSync(password,user.password);
-if(!match)
-{
-    return res.json(false);
-}
-else{
-    const token=Jwt.sign({username:username,password:password,isLoggedIn:true},'blogNode3123',{expiresIn: '1h'})
-    return res.json({message:"success",token});
-}
-}
+import jwt from 'jsonwebtoken';
+
+export const signin = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await usermodel.findOne({ username });
+
+    if (!user) {
+      // User not found
+      return res.json(false);
+    }
+
+    const match = bcrypt.compareSync(password, user.password);
+
+    if (!match) {
+      // Password doesn't match
+      return res.json(false);
+    }
+
+    // If both conditions are met, generate and return the token
+    const token = jwt.sign(
+      { username: username, isLoggedIn: true },
+      'blogNode3123',
+      { expiresIn: '1h' }
+    );
+
+    return res.json({ message: 'success', token });
+  } catch (error) {
+    console.error('Error during sign-in:', error);
+    return res.status(500).json({ message: 'Internal server error' });
+  }
+};
