638e48122755bebdad483576c82832f8a08648d9
Batoolkharraz <99608072+Batoolkharraz@users.noreply.github.com>
2023-10-24 19:54:40 +0300

    fix doctor

--- src/module/doctor/doctor.router.js
+++ src/module/doctor/doctor.router.js
@@ -10,8 +10,8 @@ import { endPoint } from "./doctor.endPoint.js";
 const router = Router();
 
 
-router.post('/',auth(endPoint.create),fileUpload(fileValidation.image).single('image'),validation(validators.createDoctor),doctorCont.createDoctor);
-router.patch('/update',auth(endPoint.update),fileUpload(fileValidation.image).single('image'),validation(validators.updateDoctor),doctorCont.updateDoctor)
+router.post('/',fileUpload(fileValidation.image).single('image'),validation(validators.createDoctor),doctorCont.createDoctor);
+router.patch('/update',fileUpload(fileValidation.image).single('image'),validation(validators.updateDoctor),doctorCont.updateDoctor)
 router.get('/',doctorCont.getAllDoctor);
 router.get('/:doctorId',doctorCont.getDoctor);
 router.get('/doctorByCategory/:categoryId',doctorCont.getDoctorByCat);
