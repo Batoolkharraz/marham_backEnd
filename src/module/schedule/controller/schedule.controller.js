04ac1162c88e891924d37cb4d60ea39950ad001c
Batoolkharraz <99608072+Batoolkharraz@users.noreply.github.com>
2023-10-31 23:28:35 +0200

    add break time 15 min

--- src/module/schedule/controller/schedule.controller.js
+++ src/module/schedule/controller/schedule.controller.js
@@ -28,9 +28,42 @@ export const createSchedule = asyncHandler(async (req, res, next) => {
 
         // Increment the current time by the duration in minutes
         currentTime.setMinutes(currentTime.getMinutes() + duration);
+
+        // Add a break time after each time slot (e.g., 15 minutes)
+        currentTime.setMinutes(currentTime.getMinutes() + 15);
     }
 
+    const existingSchedule = await scheduleModel.findOne({
+        writtenBy: req.params.docId,
+        'scheduleByDay.date': date,
+    });
 
+    if (existingSchedule) {
+        // Schedule already exists for this date, add the new data to the existing schedule
+        const scheduleDay = existingSchedule.scheduleByDay.find(
+            (day) => day.date === date
+        );
+
+        if (scheduleDay) {
+            scheduleDay.duration = duration;
+            scheduleDay.startTime = startTime;
+            scheduleDay.endTime = endTime;
+            scheduleDay.timeSlots = timeSlots;
+        } else {
+            existingSchedule.scheduleByDay.push({
+                duration: duration,
+                startTime: startTime,
+                endTime: endTime,
+                date: date,
+                timeSlots: timeSlots,
+            });
+        }
+
+        // Save the updated doctor's schedule
+        await existingSchedule.save();
+        return res.status(201).json({ existingSchedule });
+
+    }
     const schedule = await scheduleModel.findOne({ writtenBy: req.params.docId });
     if (schedule) {
         //add the new data to the scheduleByDay
