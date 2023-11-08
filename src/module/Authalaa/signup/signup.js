4d06dbde042bc30b054a934a13dfaf98f360e435
Alaa Adham <103996515+AlaaAdham@users.noreply.github.com>
2023-11-08 18:14:33 +0200

    i

--- src/module/Authalaa/signup/signup.js
+++ src/module/Authalaa/signup/signup.js
@@ -15,8 +15,15 @@ if(user2){
     return  res.json("false2");
 }
 else{
-const {secure_url,public_id}=await cloudinary.uploader.upload(`9035117_person_icon.png`,{folder:`${process.env.APP_NAME}/User`});    
+    if(role)
+    {
+        const {secure_url,public_id}=await cloudinary.uploader.upload(`9035117_person_icon.png`,{folder:`${process.env.APP_NAME}/User`});    
+        const createuser2= await usermodel.create({username, email, phone,password: hashvalue,image:{secure_url,public_id}},role);
+            return res.json({message:"from signup with role"});
+    }
+    else
+{const {secure_url,public_id}=await cloudinary.uploader.upload(`9035117_person_icon.png`,{folder:`${process.env.APP_NAME}/User`});    
 const createuser= await usermodel.create({username, email, phone,password: hashvalue,image:{secure_url,public_id}});
-    return res.json({message:"from signup"});
+    return res.json({message:"from signup"});}
 }
 }
