5077ba49dd33564edf1b9006d22ff7ec0c1a9584
Alaa Adham <103996515+AlaaAdham@users.noreply.github.com>
2023-11-08 18:25:36 +0200

    ss

--- index.js
+++ index.js
@@ -24,6 +24,6 @@ app.use('/giveme',idreturn);
 
 
 initApp(app,express);
-app.listen(3001, () => {
-    console.log('Server is running on port 3001');
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
   });
--- src/module/Authalaa/signup/signup.js
+++ src/module/Authalaa/signup/signup.js
@@ -15,15 +15,20 @@ if(user2){
     return  res.json("false2");
 }
 else{
-    if(role)
-    {
-        const {secure_url,public_id}=await cloudinary.uploader.upload(`9035117_person_icon.png`,{folder:`${process.env.APP_NAME}/User`});    
-        const createuser2= await usermodel.create({username, email, phone,password: hashvalue,image:{secure_url,public_id},role});
-            return res.json({message:"from signup with role"});
-    }
-    else
-{const {secure_url,public_id}=await cloudinary.uploader.upload(`9035117_person_icon.png`,{folder:`${process.env.APP_NAME}/User`});    
-const createuser= await usermodel.create({username, email, phone,password: hashvalue,image:{secure_url,public_id}});
-    return res.json({message:"from signup"});}
+const {secure_url,public_id}=await cloudinary.uploader.upload(`9035117_person_icon.png`,{folder:`${process.env.APP_NAME}/User`});    
+const userData = {
+    username,
+    email,
+    phone,
+    password: hashvalue,
+    image: { secure_url, public_id },
+};
+
+if (role) {
+    userData.role = role;
+}
+const createUser = await usermodel.create(userData);
+    return res.json({message:"from signup"});
+
 }
 }
