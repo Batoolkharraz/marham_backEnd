5b0760b81124266e291dbf5ab08eedb79086974e
Alaa Adham <103996515+AlaaAdham@users.noreply.github.com>
2023-11-28 14:15:01 +0200

    x

--- src/module/bato/idrouter.js
+++ src/module/bato/idrouter.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { getUser, sent } from "./email.js";
+import { getusername } from './chat.js';
 const idreturn=Router();
 idreturn.post('/userinformation',sent);
 idreturn.get('/getUser/:userId',getUser);
