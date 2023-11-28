25a3c38bf3f278e1460b51f125897d17bacc6ead
Alaa Adham <103996515+AlaaAdham@users.noreply.github.com>
2023-11-28 14:08:54 +0200

    up

---
+++ src/module/bato/chat.js
@@ -0,0 +1,13 @@
+import usermodel from '../Authalaa/DB/Usermodel.js';
+export const getusername = async (req, res) => {
+    const { email } = req.body;
+    
+    try {
+        const user = await usermodel.findOne({ email: email });
+        return res.json(user.username);
+    }
+    catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: "Internal server error" });
+    }
+}
--- src/module/bato/idrouter.js
+++ src/module/bato/idrouter.js
@@ -3,4 +3,5 @@ import { getUser, sent } from "./email.js";
 const idreturn=Router();
 idreturn.post('/userinformation',sent);
 idreturn.get('/getUser/:userId',getUser);
+idreturn.get('/username',getusername);
 export  default idreturn;
