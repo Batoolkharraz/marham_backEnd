bef4f5d61eb9f2f48a1e56125c8a2cc41b2efab0
Alaa Adham <103996515+AlaaAdham@users.noreply.github.com>
2023-11-28 14:11:53 +0200

    s

--- src/module/bato/idrouter.js
+++ src/module/bato/idrouter.js
@@ -3,5 +3,5 @@ import { getUser, sent } from "./email.js";
 const idreturn=Router();
 idreturn.post('/userinformation',sent);
 idreturn.get('/getUser/:userId',getUser);
-idreturn.get('/username',getusername);
+idreturn.post('/username',getusername);
 export  default idreturn;
