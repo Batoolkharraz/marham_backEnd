79e4e2967d21fb83a09abaf83a78aa1c9ddc40a9
Batoolkharraz <99608072+Batoolkharraz@users.noreply.github.com>
2024-01-04 20:44:41 +0200

    fix

--- src/module/Authalaa/updateinformation/routeupdate.js
+++ src/module/Authalaa/updateinformation/routeupdate.js
@@ -5,6 +5,6 @@ import fileUpload, { fileValidation } from "../../../Services/multer_cloud.js";
 const updaterouter=Router();
 updaterouter.patch('/userinformation',fileUpload(fileValidation.image).single('image'),upinfo);
 updaterouter.get('/getUser',getAllUser);
-updaterouter.get('/getNumUser/',numOfUser);
+updaterouter.get('/getNumUser/123',numOfUser);
 updaterouter.get('/getNumDoctor',numOfDoc);
 export default updaterouter;
